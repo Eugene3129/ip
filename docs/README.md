689d4772d02a2494c0abdb3e479aace4475b0468
Eugene3129 <eugene.quek03@gmail.com>
2026-08-15 22:59:02 +0800

    Give the chatbot a new name

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ernest User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Ernest.java
@@ -0,0 +1,10 @@
+public class Ernest {
+    public static void main(String[] args) {
+        String banner = " _____ ____   __    __ _____ ____ _____ \n"
+                      + "| ____|  _ \\| \\   | | ____/ ___|_   _|\n"
+                      + "| |__ | |_) ||  \\  | |  _| \\___\\| |\n"
+                      + "| |___|  _ < | | \\ | | |___ ___) || |\n"
+                      + "|_____|_| \\ |_|  \\|_|_____|____/ |_|\n";
+        System.out.println(banner);
+    }
+}
