52daa013753b2e49b1ef5599bf22eee543b18524
Eugene3129 <eugene.quek03@gmail.com>
2026-08-26 23:03:56 +0800

    Echo commands entered by the user

--- src/main/java/Ernest.java
+++ src/main/java/Ernest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ernest {
     public static void main(String[] args) {
         String h_line = "______________________________________";
@@ -13,6 +15,21 @@ public class Ernest {
         System.out.printf("Hi! I'm %s.\n", chatbot_name);
         System.out.println("How can I help you?");
         System.out.println(h_line);
+        System.out.println("(Type \"bye\" to exit the chat)");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("> ");
+                String command = scanner.nextLine();
+
+                if (command.equalsIgnoreCase("bye")) {
+                    break;
+                }
+
+                System.out.println(command); // Echo the user's command
+            }
+        }
+
         System.out.println("Bye. See you again soon!");
         System.out.println(h_line);
     }
